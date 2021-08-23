805e0503d46c63a22a22de2da0c1bbdee86f9042
Peter Oettig <peet1993@gmail.com>
2021-08-23 20:18:22 +0200

    Wrote proper build script, removed dist.

--- build.sh
+++ build.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-# usage: ./build.sh [flags]
-# asm.js release flags: -O2 --memory-init-file 0
-# wasm release flags: -O2 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1
-em++ api.cpp -Iopus/include -Lopus/.libs -lopus --pre-js preapi.js --post-js api.js -O3 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 --memory-init-file 0 -s EXPORTED_FUNCTIONS="['_free', '_malloc']" -o libopus.js
+
+set -e
+
+# Create build dir
+mkdir -p build
+cd build
+
+# If opus does not exist, build it
+if [ ! -d opus ]; then
+    # Get source
+    git clone https://github.com/xiph/opus.git opus
+    cd opus
+    git checkout tags/v1.2.1
+    rm -rf .git
+
+    # Build
+    ./autogen.sh
+    emconfigure ./configure --disable-rtcd --disable-intrinsics --disable-shared --enable-static
+    emmake make -j"$(nproc)"
+
+    cd ..
+fi
+
+# Build libopusjs
+em++ ../api.cpp -Iopus/include -Lopus/.libs -lopus --pre-js ../preapi.js --post-js ../api.js -O3 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 --memory-init-file 0 -s EXPORTED_FUNCTIONS="['_free', '_malloc']" -o libopus.js
