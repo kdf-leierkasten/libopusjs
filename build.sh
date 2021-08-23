ee5465d044b461daf7c09f8ca0d11cfa4220eea0
Peter Oettig <peet1993@gmail.com>
2021-08-23 20:42:54 +0200

    Added adaption to libopus.js for typescript.
    This is for documentation purposes. If libopus.js changed significantly, the build script will fail because of set -e

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+function on_exit_with_error {
+    echo "Build script failed because one of the commands exited with an error."
+#    rm libopus.js libopus.wasm
+}
+
 set -e
+trap on_exit_with_error ERR
+
+# Move into directory of the script first as it assumes cwd
+cd "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # Create build dir
 mkdir -p build
@@ -24,3 +33,10 @@ fi
 
 # Build libopusjs
 em++ ../api.cpp -Iopus/include -Lopus/.libs -lopus --pre-js ../preapi.js --post-js ../api.js -O3 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 --memory-init-file 0 -s EXPORTED_FUNCTIONS="['_free', '_malloc']" -o libopus.js
+
+# Insert fix to work with typescript.
+# This is for documentation purposes, might not work forever.
+# In case libopus.js changes significantly, the grep command will kill the script because of set -e
+to_replace_regex='if\(Module\["ENVIRONMENT"]!=="NODE"\)libopus=Module;$'
+grep -E "$to_replace_regex" libopus.js > /dev/null 2>&1
+sed -i -E "s/$to_replace_regex/const libopus=Module;export{libopus}/" libopus.js
